811ef4b6b90d2f9c309b63e4bfa5776e3c5c8ce8
Julian Jones <61437346+julianjones95@users.noreply.github.com>
2023-01-18 20:46:15 -0500

    Update 2d_shapes.rs
    Here, I attempted to run the simple demo.. I received the following error:
    
    142 |     pub fn spawn<'a>(&'a mut self) -> EntityCommands<'w, 's, 'a> {
        |            ^^^^^
    help: remove the extra argument
        |
    36  |     commands.spawn();
        |                   ~~
    
    After consulting the documentation [here](https://docs.rs/bevy/0.5.0/bevy/prelude/struct.Commands.html#method.spawn_bundle)
    
    I could see I should be using spawn_bundle instead of spawn()

--- examples/2d/2d_shapes.rs
+++ examples/2d/2d_shapes.rs
@@ -17,7 +17,7 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     // Rectangle
-    commands.spawn(SpriteBundle {
+    commands.spawn_bundle(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
             custom_size: Some(Vec2::new(50.0, 100.0)),
@@ -27,7 +27,7 @@ fn setup(
     });
 
     // Circle
-    commands.spawn(MaterialMesh2dBundle {
+    commands.spawn_bundle(MaterialMesh2dBundle {
         mesh: meshes.add(shape::Circle::new(50.).into()).into(),
         material: materials.add(ColorMaterial::from(Color::PURPLE)),
         transform: Transform::from_translation(Vec3::new(-100., 0., 0.)),
@@ -35,7 +35,7 @@ fn setup(
     });
 
     // Hexagon
-    commands.spawn(MaterialMesh2dBundle {
+    commands.spawn_bundle(MaterialMesh2dBundle {
         mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
         material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
         transform: Transform::from_translation(Vec3::new(100., 0., 0.)),
